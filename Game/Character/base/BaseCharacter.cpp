a684a64aea47dc39836f045778e4d0429240b5b9
OikawaYuma <k022g0016@g.neec.ac.jp>
2024-08-31 14:33:46 +0900

    色々な便利機能の移動

---
+++ Game/Character/base/BaseCharacter.cpp
@@ -0,0 +1,23 @@
+#include "BaseCharacter.h"
+
+
+
+void BaseCharacter::Init()
+{
+	worldTransform_.Initialize();
+}
+
+void BaseCharacter::Update()
+{
+	for (Object3d* object : objects_) {
+		object->Update();
+	}
+	worldTransform_.UpdateMatrix();
+}
+
+void BaseCharacter::Draw(Camera* camera)
+{
+	for (Object3d* object : objects_) {
+		object->Draw(skinTex_,camera);
+	}
+}
